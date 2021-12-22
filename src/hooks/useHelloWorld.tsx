81d1a4ea19605bb5c6c1ff97e328e7dd3a854b15
Corey O'Donnell <me@coreyodonnell.com>
2021-12-21 21:59:33 -0500

    feat(vite): Convert to lib build, add placeholder lib

---
+++ src/hooks/useHelloWorld.tsx
@@ -0,0 +1,3 @@
+import { FC } from "react";
+
+export const useHelloWorld: FC = () => <div>Hello World</div>;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react({ exclude: /\.stories\.(t|j)sx?$/ })],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "/src/index.ts"),
+      name: "decentralize",
+      fileName: (format) => `decentralize.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: { globals: { react: "React", "react-dom": "ReactDOM" } },
+    },
+  },
+});
